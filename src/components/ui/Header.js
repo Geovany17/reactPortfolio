67ed1ccadd3596daa4ca558ec06901e035519131
Geovany Cardoza <geoyd17@gmail.com>
2020-10-31 10:03:06 -0400

    adding components folder and ui folder with a starting header file

---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+
+export default function Header(props) {
+  return (
+    <AppBar>
+      <ToolBar>My Portfolio</ToolBar>
+    </AppBar>
+  );
+}
