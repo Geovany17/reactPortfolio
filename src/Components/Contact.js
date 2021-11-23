24e9e0aef2d246254857360f8ae923535122888b
Geovany Cardoza <geoyd17@gmail.com>
2021-11-23 00:42:20 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -14,19 +14,7 @@ class Contact extends Component {
     }
 
     return (
-      <section id="contact">
-        <div className="row section-head">
-          <div className="two columns header-col">
-            <h1>
-              <span>Get In Touch.</span>
-            </h1>
-          </div>
-
-          <div className="ten columns">
-            <p className="lead">{message}</p>
-          </div>
-        </div>
-
+     
           <aside className="four columns footer-widgets">
             <div className="widget widget_contact">
               <h4>Address and Phone</h4>
